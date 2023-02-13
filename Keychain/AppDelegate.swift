dd76c948bc9f4497abcb4e40f319236cb7d56816
Gustavo Braghin <gugabraghin@gmail.com>
2023-02-13 11:06:00 -0300

    Initial Commit

---
+++ Keychain/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Keychain
+//
+//  Created by Gustavo da Silva Braghin on 13/02/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Keychain/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Keychain
+//
+//  Created by Gustavo da Silva Braghin on 13/02/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
